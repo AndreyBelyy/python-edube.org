09b54743c97c20dce40905bb4bd8430d3ffaacbd
aegar <andreybelyyw@gmail.com>
2022-12-08 12:19:52 -0600

    Full program

--- Functions.py
+++ Functions.py
@@ -1,5 +1,15 @@
-def message(put):
-    #print(input("Hello"))
-    print(("Hello, ", put))
-put = input()
-message(put)
+def privet_func(name):
+    """Print Privetstvie"""
+    print("Hello. \n  _________ \n All best \n _________ \n  With Love" + " to " + name )
+def summa(x, z):
+     return x + z
+
+def factorial(o):
+    """Calculate Factorial of o"""
+    otvet = 1
+    for i in range (1, o + 1):
+        otvet = otvet * i
+    return otvet
+
+for i in range(1, 10):
+    print(str(i) + "!\t = " + str(factorial(i)))

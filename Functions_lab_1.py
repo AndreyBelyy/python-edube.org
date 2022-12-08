751821f72de44289632bb1195179dcf372f9ce29
aegar <andreybelyyw@gmail.com>
2022-12-08 17:08:17 -0600

    Lab 1, part2

--- Functions_lab_1.py
+++ Functions_lab_1.py
@@ -16,6 +16,12 @@ vys_year = is_year_leap(year)
 
 
 # Part 2
+# Your task is to write and test a function which takes two arguments (a year and a month) and
+# returns the number of days for the given month/year pair
+# (while only February is sensitive to the year value, your function should be universal).
+
+# The initial part of the function is ready.
+# Now, convince the function to return None if its arguments don't make sense.
 
 def days_in_month(year, month):
     lm = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]

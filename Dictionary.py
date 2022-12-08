b96635a56fe967d9c4921b9f2d0610bbb33c097d
aegar <andreybelyyw@gmail.com>
2022-12-07 22:26:06 -0600

    Full program

--- Dictionary.py
+++ Dictionary.py
@@ -3,5 +3,21 @@ fruites = {
     'pineapple': 'yellow',
     'banana': 'yellow',
     'grape': 'green',
+    'kg': 10
      }
 print(fruites['apple'])
+
+fruites['peach'] = 'yellow'
+print(fruites)
+
+del fruites['grape']
+print(fruites)
+
+fruites['kg'] = fruites['kg'] + 2
+while fruites['kg'] < 20:
+    fruites['kg'] = fruites['kg'] + 1
+    print(fruites)
+
+print(fruites.values())
+print(fruites.keys())
+print(fruites)
